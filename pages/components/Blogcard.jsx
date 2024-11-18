a13b217f3e8a8380dc8fe2bc3fc6c0511c3d7157
unknown <aaradhya.korde@gmail.com>
2024-11-18 23:08:55 +0530

    chakdephatte

--- pages/components/Blogcard.jsx
+++ pages/components/Blogcard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BackgroundGradient } from "./ui/background-gradient";
+import BackgroundGradient from "./ui/background-gradient";
 import Image from "next/image";
 import Link from "next/link";
 
